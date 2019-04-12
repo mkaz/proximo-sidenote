9ce3de428ba8d2f39c72ed1cf6bbf59507f42a42
Marcus Kazmierczak <marcus@mkaz.com>
2019-04-11 20:29:29 -0700

    Add transforms, fix whitespace

--- proximo-sidenote.js
+++ proximo-sidenote.js
@@ -1,53 +1,65 @@
 
 ( function( blocks, editor, element ) {
     var el = element.createElement;
-	var Fragment = element.Fragment;
-	var RichText = editor.RichText;
+    var Fragment = element.Fragment;
+    var RichText = editor.RichText;
 
     blocks.registerBlockType( 'proximo/sidenote', {
         title: 'Sidenote',
         icon: 'admin-post',
         category: 'layout',
 
-		attributes: {
-			content: {
-				type: 'array',
-				source: 'children',
-				selector: 'span',
-			},
-		},
+        attributes: {
+            content: {
+                type: 'array',
+                source: 'children',
+                selector: 'span',
+            },
+        },
+
+        transforms: {
+            from: [{
+                type: 'block',
+                blocks: [ 'core/paragraph' ],
+                transform: function ( attributes ) {
+                    return blocks.createBlock( 'proximo/sidenote', {
+                        content: attributes.content,
+                    } );
+                }
+            }]
+        },
 
         edit: function( props ) {
-			var content = props.attributes.content;
-			function onChangeContent( newContent ) {
-				props.setAttributes( { content: newContent } );
-			}
+            var content = props.attributes.content;
+            function onChangeContent( newContent ) {
+                props.setAttributes( { content: newContent } );
+            }
 
             return el(
-				RichText,
+                RichText,
                 {
-					tagName: 'span',
-					className: props.className,
-					onChange: onChangeContent,
-					value: content,
-				}
+                    tagName: 'span',
+                    className: props.className,
+                    onChange: onChangeContent,
+                    value: content,
+                }
             );
         },
 
         save: function( props ) {
             return el(
-				RichText.Content,
-				{
-					tagName: 'span',
-					className: 'sidenote',
-					value: props.attributes.content,
-				}
+                RichText.Content,
+                {
+                    tagName: 'span',
+                    className: 'sidenote',
+                    value: props.attributes.content,
+                }
             );
         },
     } );
 }(
     window.wp.blocks,
-	window.wp.editor,
+    window.wp.editor,
     window.wp.element,
 ) );
 
